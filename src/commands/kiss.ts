73bf41bc52d0443a2115afc427089792d29499e3
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-10-07 20:33:08 -0300

    more kiss

--- src/commands/kiss.ts
+++ src/commands/kiss.ts
@@ -7,8 +7,16 @@ const data = {
     description: 'Send a kiss'
 }
 
+const url = [
+    "https://tenor.com/view/cat-kissing-mwah-cat-kissing-camera-gif-21041612",
+    "https://tenor.com/view/racoon-cat-gif-10153591",
+    "https://tenor.com/view/raccoon-dog-kisses-gif-15175501",
+    "https://tenor.com/view/dog-cat-kiss-gif-5527386",
+    "https://tenor.com/view/cat-dog-play-gif-4728498"
+]
+
 function execute(ctx: Context) {
-    ctx.reply("https://tenor.com/view/cat-kissing-mwah-cat-kissing-camera-gif-21041612")
+    ctx.reply(url[Math.floor(Math.random() * url.length)])
 }
 
 export default {
